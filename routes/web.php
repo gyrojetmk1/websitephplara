b9f7d1dd60226c13d125930402d7d500ac58d7e4
gyrojetmk1 <109605379+gyrojetmk1@users.noreply.github.com>
2023-09-14 21:25:55 -0600

    update again

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use App\Http\Controllers\registercontroller;
+use App\Http\Controllers\sessioncontroller;
+use App\Http\Controllers\uploadcontroller;
+use App\Http\Controllers\mainpagescontroller;
 use App\Models\cachepage;
+use App\Models\posts;
+use App\Livewire\Counter;
+use App\Http\Middleware\adminverify;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,14 +24,25 @@ use App\Models\cachepage;
 |
 */
 
-Route::get('/', function () {
-    return view('home', [
-        'home' => cachepage::cache('home')
-    ]);
-});
+Route::get('/', [mainpagescontroller::class, 'home']);
 
-Route::get('/tests', function () {
-    return view('test', [
-        'test' => cachepage::cache('test')
-    ]);
-});
+Route::get('/tests', [mainpagescontroller::class, 'tests']);
+
+Route::get('/register', [registercontroller::class, 'render'])->middleware('guest');
+
+Route::post('/register', [registercontroller::class, 'resgister'])->middleware('guest');
+
+Route::get('/logout', [sessioncontroller::class, 'logout']);
+
+Route::get('/login', [sessioncontroller::class, 'render'])->middleware('guest');
+
+Route::post('/login', [sessioncontroller::class, 'login'])->middleware('guest');
+
+Route::get('/admin/upload', [uploadcontroller::class, 'uploadpg'])->middleware('notguest', 'admin');
+
+Route::post('/admin/upload', [uploadcontroller::class, 'upload'])->middleware('notguest', 'admin');Route::get('/admin/upload', [uploadcontroller::class, 'uploadpg'])->middleware('notguest', 'admin');
+
+Route::post('/admin/delete', [uploadcontroller::class, 'delete'])->middleware('notguest', 'admin');
+
+Route::get('/admin/delete', [uploadcontroller::class, 'deletepg'])->middleware('notguest', 'admin');
+
